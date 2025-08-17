9b1a519e0e94cd797c530f026bcaee01de6cddd4
Domaakshithareddy <akshithareddydoma7@gmail.com>
2025-08-17 13:12:18 +0530

    add main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,44 @@
 from fastapi import FastAPI
-from agents.responder_agent import responder
+import requests, time
+import threading
 
-app = FastAPI(title="AutoOps API")
+app = FastAPI()
 
-# Include your responder routes
-app.include_router(responder.router)
+MONITOR_URL='http://localhost:8000/simulate_build'
+OPTIMIZER_URL="http://localhost:8500/get_best_action"
+RESPONDER_URL="http://localhost:8600/suggest_fix"
 
-@app.get("/")
-async def root():
-    return {"message": "AutoOps API is running"}
+def autoops_loop(interval=60):
+    while True:
+        try:
+            monitor_resp=requests.get(MONITOR_URL).json()
+            print('[Monitor]->',monitor_resp)
+            if monitor_resp['status']=='failed':
+                duration=monitor_resp['duration']
+                optimizer_resp=requests.post(
+                    OPTIMIZER_URL,
+                    json={'duration':duration,'success':0}
+                ).json()
+                print('[Optimizer]->',optimizer_resp)
+                
+                issue='build failure due to memory limits'
+                responder_resp=requests.post(
+                    RESPONDER_URL,
+                    json={'issue':issue}
+                ).json()
+                print('[Responder]->',responder_resp)
+            else:
+                print('[Monitor] Build secceeded. No action meeded')
+        
+        except Exception as e:
+            print('[Controller] error:',e)
+        
+        time.sleep(interval)
+        
+@app.on_event('startup')
+def start_loop():
+    threading.Thread(target=autoops_loop,args=(60,),daemon=True).start()
+    
+@app.get('/')
+def root():
+    return {'status':'controller running (loop mode)'}
