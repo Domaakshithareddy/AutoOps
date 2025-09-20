bc4ba4aed9c0a9a05d33aac68effcb890b4c84d5
Domaakshithareddy <akshithareddydoma7@gmail.com>
2025-09-20 21:27:47 +0530

    dqn

--- agents/optimizer_agent/dqn_agent.py
+++ agents/optimizer_agent/dqn_agent.py
@@ -4,9 +4,7 @@ from environment import BuildOptimizerEnv
 env=BuildOptimizerEnv()
 
 model=DQN('MlpPolicy',env,verbose=1)
-
 model.learn(total_timesteps=10000)
-
 model.save('agents/optimizer_agent/model/dqn_build_optimizer')
 
 print('Training complete. Model saved.')
